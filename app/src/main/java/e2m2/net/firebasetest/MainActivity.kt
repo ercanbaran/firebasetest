c9b4503c038bfb161c746badada86cf0028ffcb0
Ercan Baran <ercan.baran@e2m2.net>
2019-07-12 15:14:35 +0300

    libs updated

--- app/src/main/java/e2m2/net/firebasetest/MainActivity.kt
+++ app/src/main/java/e2m2/net/firebasetest/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.coroutines.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        //Socket urls
+        //https://gist.github.com/Silverbaq/a14fe6b3ec57703e8cc1a63b59605876
+        //https://www.baeldung.com/a-guide-to-java-sockets
+
+        //LocationLib
+        //https://github.com/mrmans0n/smart-location-lib
 
 
         FirebaseInstanceId.getInstance().instanceId
@@ -34,6 +43,49 @@ class MainActivity : AppCompatActivity() {
 
                 Log.d("------>","registered")
             }
+//
+//        GlobalScope.launch {
+//            runHello()
+//            async {
+//
+//            }
+//        }
+
+
+//        repeat(100_000) { // launch a lot of coroutines
+////            GlobalScope.launch {
+////                delay(1000L)
+////                println("------>.")
+////            }
+//            thread {
+//                Thread.sleep(1000L)
+//                println("------>.")
+//
+//            }
+//        }
+
+        GlobalScope.launch {
+            repeat(1000) { i ->
+                println("I'm sleeping $i ...")
+                delay(500L)
+            }
+
+            delay(1300L) // just quit after delay
+
+        }
+
+
+
+    }
+
+    suspend fun runHello()
+    {
+        val job = GlobalScope.launch { // launch a new coroutine and keep a reference to its Job
+            delay(1000L)
+            println("----World!")
+        }
+        println("Hello,")
+        job.join() // wait until child coroutine completes
     }
 
     override fun onResume() {
