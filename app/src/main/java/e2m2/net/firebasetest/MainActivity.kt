282cfc0eb4cf20dda239450f7381b6a17cba90ae
Ercan Baran <ercan.baran@e2m2.net>
2019-07-12 14:23:32 +0300

    push message implementation

--- app/src/main/java/e2m2/net/firebasetest/MainActivity.kt
+++ app/src/main/java/e2m2/net/firebasetest/MainActivity.kt
@@ -2,11 +2,45 @@ package e2m2.net.firebasetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("----->", "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                Log.d("------->", token)
+            })
+
+
+        FirebaseMessaging.getInstance().subscribeToTopic("weather")
+            .addOnCompleteListener {
+
+                Log.d("------>","registered")
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(intent.hasExtra("kombiID"))
+        {
+            Log.d("------->", "opened from notification")
+        }
     }
 }
--- app/src/main/java/e2m2/net/firebasetest/MyFirebaseMessagingService.kt
+++ app/src/main/java/e2m2/net/firebasetest/MyFirebaseMessagingService.kt
@@ -1,8 +1,46 @@
 package e2m2.net.firebasetest
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService()
 {
 
+    val TAG = "-------->"
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String?) {
+        Log.d("--->", "Refreshed token: $token")
+
+        //sendRegistrationToServer(token)
+    }
+
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage?.from}")
+
+        // Check if message contains a data payload.
+        remoteMessage?.data?.isNotEmpty()?.let {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+
+
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage?.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
 }
